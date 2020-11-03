8544016936de6ca06b2631ab621b5f1cc9726455
dolooo <72699696+dolooo@users.noreply.github.com>
2020-11-03 23:45:15 +0100

    Update zad6.cpp

--- lab2/zad6.cpp
+++ lab2/zad6.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-template < typename T > 
+template < typename T >
 void zamiana(T **a, T **b)
 {
 	T* temp = *a;
@@ -16,6 +16,8 @@ int main()
 {
 	double a = 0, b = 0;
 	string wyraz1, wyraz2;
+	long tab1[4] = { 1,2,3,4 };
+	long tab2[4] = { 5,6,7,8 };
 
 	cout << "Podaj a: ";    cin >> a;
 	cout << "Podaj b: ";    cin >> b;
@@ -26,12 +28,17 @@ int main()
 	double *wsk_b = &b;
 	string *wsk_1 = &wyraz1;
 	string *wsk_2 = &wyraz2;
+	long *wsk_tab1 = tab1;
+	long *wsk_tab2 = tab2;
 
 	zamiana(&wsk_a, &wsk_b);
 	zamiana(&wsk_1, &wsk_2);
+	zamiana(&wsk_tab1, &wsk_tab2);
 
 	cout << "Po zamianie: " << endl;
 	cout << "a: " << *wsk_a << "   " << "b: " << *wsk_b << endl;
 	cout << "Wyraz 1: " << *wsk_1 << "   " << "Wyraz 2: " << *wsk_2 << endl;
+	cout << "Pierwszy wyraz tablicy 1: " << *wsk_tab1 << endl;
+	cout << "Pierwszy wyraz tablicy 2: " << *wsk_tab2 << endl;
 
 }
