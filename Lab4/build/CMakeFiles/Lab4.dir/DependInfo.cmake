4ac31c63b7ab6134c94408a86b9900e9da7b4047
dolooo <72699696+dolooo@users.noreply.github.com>
2020-12-03 14:12:09 +0100

    Add files via upload

---
+++ Lab4/build/CMakeFiles/Lab4.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/sdole/OneDrive/Desktop/3 sem/jipp/jipp_lab/Lab4/src/func.cpp" "C:/Users/sdole/OneDrive/Desktop/3 sem/jipp/jipp_lab/Lab4/build/CMakeFiles/Lab4.dir/src/func.cpp.obj"
+  "C:/Users/sdole/OneDrive/Desktop/3 sem/jipp/jipp_lab/Lab4/src/main.cpp" "C:/Users/sdole/OneDrive/Desktop/3 sem/jipp/jipp_lab/Lab4/build/CMakeFiles/Lab4.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  ".././src"
+  ".././libs"
+  ".././tests"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
