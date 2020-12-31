e6be2a75ba774b968e2b86bf2419dac321e05edc
dolooo <72699696+dolooo@users.noreply.github.com>
2020-12-31 16:10:39 +0100

    Update main.cpp

--- Lab6/src/main.cpp
+++ Lab6/src/main.cpp
@@ -5,15 +5,15 @@
 
 using namespace std;
 
-	Employee e1("1", "John", "Travolta", "1", "Administracja");
-	Employee e2("2", "Adam", "Sandler", "2", "BHP");
-	Employee e3("3", "Penelope", "Cruz", "1", "Administracja");
+	Employee e1("1", "John", "Travolta", "1", "Administration");
+	Employee e2("2", "Adam", "Sandler", "2", "Health and Safety");
+	Employee e3("3", "Penelope", "Cruz", "1", "Administration");
 	Employee e4("4", "Marcin", "Najman", "4", "IT");
 	Employee e5("5", "Elon", "Musk", "3", "Transport");
-	Employee e6("6", "Leonardo", "Dicaprio", "5", "Sprzedaz");
-	Employee e7("7", "Marilyn", "Monroe", "1", "Administracja");
+	Employee e6("6", "Leonardo", "Dicaprio", "5", "Sales");
+	Employee e7("7", "Marilyn", "Monroe", "1", "Administration");
 	Employee e8("8", "Freddie", "Mercury", "3", "Transport");
-	Employee e9("9", "Janusz", "Kowalski", "5", "Sprzedaz");
+	Employee e9("9", "Janusz", "Kowalski", "5", "Sales");
 	Employee e10("10", "Grazyna", "Kowalska", "4", "IT");
 
 int main()
@@ -29,11 +29,11 @@ int main()
 	test.add(Employee(e8), "3", 2560);
 	test.add(Employee(e9), "5", 8990);
 	test.add(Employee(e10),"4", 7760);
-	test.printDepartment("1"); //administracja
-	test.printDepartment("2"); //BHP
+	test.printDepartment("1"); //administration
+	test.printDepartment("2"); //Health and safety
 	test.printDepartment("3"); //transport
 	test.printDepartment("4"); //IT
-	test.printDepartment("5"); //sprzedaz
+	test.printDepartment("5"); //sales
 	test.changeSalary("1", 4444);
 	test.changeSalary("9", 6666);
 	test.changeSalary("10", 9999);
